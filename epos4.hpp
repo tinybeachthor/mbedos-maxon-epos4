e473a2d4b72090a061e2d00175a8f0c9c07e0878
Martin Toman <whomenope@outlook.com>
2019-12-15 13:32:12 +0100

    Add epos4 header

---
+++ epos4.hpp
@@ -0,0 +1,25 @@
+#include "mbed.h"
+
+namespace epos4 {
+
+  enum State : uint8_t {
+    NotReadyToSwitchOn  = 0,
+    SwitchOnDisabled    = 1,
+    ReadyToSwitchOn     = 2,
+    SwitchedOn          = 3,
+
+    OperationEnabled    = 4,
+    QuickStopActive     = 5,
+
+    FaultReactionActive = 6,
+    Fault               = 7,
+
+    Unknown             = 255,
+  };
+
+  extern void init (PinName rx, PinName tx);
+  extern void start ();
+  extern void stop ();
+
+  extern uint8_t getState ();
+}
